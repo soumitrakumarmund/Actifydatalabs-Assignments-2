56faa9ed3cb427aa2c4e953726d1f004045fb726
soumitrakumarmund <soumitramund93@.gmai.com>
2020-08-17 11:04:38 +0530

    Developed for Chat Communications

---
+++ src/main/java/com/chatapp/ChatApplicationWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationWebsocketApplication.class, args);
+	}
+
+}
